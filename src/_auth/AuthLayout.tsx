aad3586673d2adb132dd9ddd9bbef3fc4e6e8534
Amit Biswas <amit.142biswas@gmail.com>
2024-04-09 21:40:12 +0530

    auth pages created

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -9,9 +9,13 @@ const AuthLayout = () => {
         <Navigate to="/" />
       ) : (
         <>
-          <section className="flex flex-1 justify-center">
+          <section className="flex flex-1 justify-center items-center">
             <Outlet />
           </section>
+
+          <img src="/public/images/side-img.svg" alt="logo" 
+          className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
         </>
       )}
     </>
